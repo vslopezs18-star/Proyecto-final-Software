a6aa3d5881a605d647bb90439bdbc0a9314cbd96
vslopezs18-star <vslopezs18@gmail.com>
2025-11-11 22:00:25 -0600

    .

--- Borrador.py
+++ Borrador.py
@@ -7,20 +7,31 @@
 #Paso numero dos: Dividir esos gastos en el sistema
 #Paso numero tres: Definir en que distribuira exactamente su dinero en cada seccion 
 
-print("Bienvenido/a a PapoiMoney \n Descubre la forma más inteligente y sencilla de manejar tu dinero con el sistema 50-30-20: \n 50% para Necesidades Esenciales (vivienda, comida, transporte). \n 30% para Deseos (entretenimiento, gastos personales). \n 20% para Ahorro o Deudas. \n Automatiza tu presupuesto, evita gastos innecesarios y toma el control de tu bienestar financiero. ¡Comencemos a planificar!")
+print("Bienvenido/a a PapoiMoney \n Descubre la forma más inteligente y sencilla de manejar tu dinero con el sistema 50-30-20: \n 50% para Necesidades Esenciales (vivienda, comida, transporte). \n 30% para Deseos (entretenimiento, gastos personales). \n 20% para Ahorro. \n Automatiza tu presupuesto, evita gastos innecesarios y toma el control de tu bienestar financiero. ¡Comencemos a planificar!")
+Sueldo = float(input("Para empezar ingrese la cantidad de dinero que posee: $"))
 
-Sueldo = 0
-if Sueldo>0:
-        Sueldo.append(float(input("Para empezar ingrese la cantidad de dinero que posee: ")))
-        print(Sueldo)
-        
-"""print("La distribucion esta dada por: \n 50% para necesidades esenciales, como vivienda, alimentación y transporte. \n 30% para deseos o gastos personales, como entretenimiento o compras no esenciales. \n 20% para ahorro o pago de deudas.")
+Necesidades = []
+Deseos = []
+Ahorro = []
 
 distribucion=True
 while distribucion is True:
-    eleccion=int(input("Digite la opcion a elegir (1-3): "))
-    if eleccion == 1:
-        print()"""
+    sección = int(input("Secciones: \n 1. Necesidades Esenciales (ej. vivienda, comida, transporte), \n 2. Deseos (entretenimiento, gastos personales). \n 3. Ahorro \n 4. Salir \n Digite la sección a la que desea entrar: "))
+    if sección == 4:
+        distribucion = False
+    elif sección == 1:
+            eleccion=int(input("1.Agregar categorías, 2.Consular categorías, 3.Modificar categorías, 4. Borrar categorías "))
+            if eleccion == 1:
+            Necesidades.append("Ingrese la categoría a agregar: ")
+            elif eleccion == 2:
+            print(Necesidades)
+        elif eleccion == 3:
+            # nuevo = input("Digite el nombre nuevo: ")
+            if len(Necesidades) == 0:
+                print("Actualmente no hay categorías en Necesidades")
+            else:
+                for v in range(len(Necesidades)):
+                    print(f"Actualmente hay {len(Necesidades)} en Necesidades")
         
 """while menu_iniciado: 
     opción = int(input("1.Agregar, 2.Consular, 3.Modificar, 4. Borrar, 5. Salir"))
